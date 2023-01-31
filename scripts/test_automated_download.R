0c59cb01b808c7e276edae4b0640d27ac99474ef
jkelly3rd <jkelly3rd@gmail.com>
2023-01-31 11:36:56 -0500

    Update Jan29 data

--- scripts/test_automated_download.R
+++ scripts/test_automated_download.R
@@ -1,44 +1,54 @@
 # load required packages
 library(rvest)
 library(tidyverse)
-library(xml2)
-library(XML)
+library(stringr)
+# library(xml2)
+# library(XML)
 
 # download the webpage
 # Load the webpage
-webpage <- read_html("https://cityofoakland2.app.box.com/s/xqloqg6rpaljxz6h0cajle6skmoea5ct/folder/188715986606")
-webpage <- read_html("https://cityofoakland2.app.box.com/s/xqloqg6rpaljxz6h0cajle6skmoea5ct")
-
+webpage_citywide <- read_html("https://cityofoakland2.app.box.com/s/xqloqg6rpaljxz6h0cajle6skmoea5ct/folder/188715986606")
+# webpage <- read_html("https://cityofoakland2.app.box.com/s/xqloqg6rpaljxz6h0cajle6skmoea5ct")
+webpage_area1 <- read_html("https://cityofoakland2.app.box.com/s/2gmg1912khb9t6nhalhl1rg6gscn4y86")
+webpage_area2 <- read_html("https://cityofoakland2.app.box.com/s/xfa5h508r77eysje2etevwio80e1o0gk")
+webpage_area3 <- read_html("https://cityofoakland2.app.box.com/s/x81lhgwlpsj7o8gn4iiw9oujhpfjgql9")
+webpage_area4 <- read_html("https://cityofoakland2.app.box.com/s/ih1xz8vyo7btuhu8fb5qhy4wzbapy1t8")
+webpage_area5 <- read_html("https://cityofoakland2.app.box.com/s/yfpjz8dw2zoum7cxoml0h908b6xme8ah")
 
 # this works to get the names and id numbers of files
 test_files_list <- webpage %>% html_elements(".is-loading") %>% html_text2()
 
-# we need to capture everything starting with this
-# keeping the part after the opening bracket
-# ]}},{"typedID":"f_
-test_files_list <- gsub('.*\\]\\}\\},\\{"typedID"\\:"f_', '{"typedID":"f_', test_files_list) 
-
-# gsub(".*\\.", "", x)
-
-# we need to drop everything after this and replace with just close bracket
-# ],"folder":{"typedID":"d_
-test_files_list <- gsub(',"folder"\\:\\{"typedID"\\:"d_.*', "", test_files_list) 
 
 
-# Remove all \ from the string
-#test_files_list <- gsub('\\"', "'", test_files_list) 
-#test_files_list <- gsub("'", '"', test_files_list) 
-
-library("rjson")
-result <- toJSON(test_files_list)
-Writing the JSON object created to a file named "myJSON". We use write() function to do so.
+# Define the input string
+input_string <- test_files_list
 
 
-write(test_files_list, "test_files_list.json")
-write(test_files_list, "test_files_list.txt")
+
+# FIRST TO GET FILE IDs
+# Extract the 12 characters after each instance of the pattern
+extracted_text <- str_extract_all(input_string, "\\{\\\"typedID\\\":\\\"f_.{12}")
+# Convert the extracted text to a character vector
+extracted_text <- unlist(extracted_text) %>% as.data.frame %>% rename("file_id" = ".")
+# remove the extraneous text
+extracted_text$file_id <- substr(extracted_text$file_id,15,26)
+
+# SECOND TO GET FILE NAMES
+# Extract the 12 characters after each instance of the pattern
+extracted_text2 <- str_extract_all(input_string, ",\"extension\":\"pdf\",\"name\":\".{75}")
+# Convert the extracted text to a character vector
+extracted_text2 <- unlist(extracted_text2) %>% as.data.frame %>% rename("file_name" = ".")
+# remove the extraneous text
+extracted_text2$file_name <- substr(extracted_text2$file_name,28,102)
+# remove the extraneous text
+extracted_text2$file_name <- gsub("pdf.*","pdf",extracted_text2$file_name)
+
+files_citywide <- cbind(extracted_text,extracted_text2) %>% filter
+files_citywide <- files_citywide[str_detect(files_citywide$file_name, "Weekly Crime Report"), ]
+files_citywide[1,1]
+files_citywide[1,2]
 
 
-result <- fromJSON(test_files_list)
-json_data_frame <- as.data.frame(result)
-library(sf)
-result2 <- st_read("test_files_list.json")
+
+
+
